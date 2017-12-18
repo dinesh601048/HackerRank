6ddc878bc35d0648e060ba098b8984e08eb46515
Dinesh Nadimpalli <dinesh601048@users.noreply.github.com>
2017-12-18 11:43:59 +0530

    C++ Solution

--- Making Anagrams.cpp
+++ Making Anagrams.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // ==================================================
 // Problem  :   Making Anagrams
 // Score    :   30/30
@@ -7,41 +7,6 @@
 
 
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int makingAnagrams(string s1, string s2){
-    int s[26]={0};
-    int sum=0;
-    for(int i=0;i<s1.length();i++){
-       s[s1[i]-'a']++; 
-    }
-    for(int i=0;i<s2.length();i++){
-        s[s2[i]-'a']--;
-    }
-    for(int i=0;i<26;i++){
-        sum += abs(s[i]);
-    }
-    return sum;// Complete this function
-}
-
-int main() {
-    string s1;
-    cin >> s1;
-    string s2;
-    cin >> s2;
-    int result = makingAnagrams(s1, s2);
-    cout << result << endl;
-    return 0;
-=======
-// ==================================================
-// Problem  :   Making Anagrams
-// Score    :   15 /15
-// Language :   C++
-// ==================================================
-
-
 
 #include <bits/stdc++.h>
 
@@ -70,5 +35,4 @@ int main() {
     int result = makingAnagrams(s1, s2);
     cout << result << endl;
     return 0;
->>>>>>> 9b7f3666ea92aebc19451542136c918b931974c3
 }
